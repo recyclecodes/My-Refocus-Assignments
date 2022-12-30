dcfebd38ba1c99fd363f53f7e4a352c377f31769
Godfrey Anthony T. Serojales <34874072+recyclecodes@users.noreply.github.com>
2022-12-30 14:08:23 +0800

    Revert "Automated Testing for BMI Calculator"
    This reverts commit f8c93e720e8b3b2e66d73da4a3635d8005b447fe.

--- automated_testing/bmi_calculator.test.js
+++
@@ -1,6 +0,0 @@
-const calculateBMI = require("./bmi_calculator");
-
-test("Check if 1.63m height and 55kg weight is 20.7 and is considered normal", () => 
-{
-    expect(calculateBMI(1.63, 55)).toBe("Your Body Mass index 20.7. This is considered normal.");
-});
